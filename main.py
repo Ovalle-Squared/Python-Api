b713d8816ad141cc6661607bc11b7b93c59544b7
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-02-13 10:00:56 -0600

    added try-except to help debug, post method works w/ raw json

--- main.py
+++ main.py
@@ -15,28 +15,29 @@ parser = reqparse.RequestParser()
 
 
 class StudentsList(Resource):
-  def get(self):
-    return STUDENTS
+    def get(self):
+        return STUDENTS
 
-  def post(self):
-    parser.add_argument("name")
-    parser.add_argument("age")
-    parser.add_argument("spec")
-    args = parser.parse_args()
-    student_id = int(max(STUDENTS.keys())) + 1
-    student_id = '%i' % student_id
-    STUDENTS[student_id] = {
-        "name": args["name"],
-        "age": args["age"],
-        "spec": args["spec"],
-    }
-    return STUDENTS[student_id], 201
+    def post(self):
+        try:
+            parser.add_argument("name", type=str, required=True, help="Name cannot be left blank!")
+            parser.add_argument("age", type=int, required=True, help="Age cannot be left blank!")
+            parser.add_argument("spec", type=str, required=True, help="Specialization cannot be left blank!")
+            args = parser.parse_args()
+            student_id = int(max(STUDENTS.keys())) + 1
+            student_id = '%i' % student_id
+            STUDENTS[student_id] = {
+                "name": args["name"],
+                "age": args["age"],
+                "spec": args["spec"],
+            }
+            return STUDENTS[student_id], 201
+        except Exception as e:
+            print("An error occurred:", e)
+            return {"error": str(e)}, 400
 
 
 api.add_resource(StudentsList, '/students/')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
